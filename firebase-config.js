b1d023561a807f875bc55306802be27947fbb7b2
Vishnu <vishnunandhu199@gmail.com>
2026-07-29 23:04:22 +0530

    Update Firebase configuration details

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA1Y7CGL174-Fk_7LGK4DvGeOu-FktIeE",
+  apiKey: "AIzaSyAlY7CGLl174-Fk_7LGK4DvGeOu-FktIeE",
   authDomain: "app-forge-f7a07.firebaseapp.com",
   projectId: "app-forge-f7a07",
-  storageBucket: "app-forge-f7a07.appspot.com",
-  messagingSenderId: "1579204050538",
-  appId: "1:1579204050538:web:5e52487a75730d958e2413"
+  storageBucket: "app-forge-f7a07.firebasestorage.app",
+  messagingSenderId: "579204050538",
+  appId: "1:579204050538:web:5e52487a75730d958e2413"
 };
-
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
