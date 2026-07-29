6c16c9059a2031fed435659e84b0ec9a5e377993
Vishnu <vishnunandhu199@gmail.com>
2026-07-29 23:07:14 +0530

    Update Firebase API key and fix formatting

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAlY7CGLl174-Fk_7LGK4DvGeOu-FktIeE",
+  apiKey: "AIzaSyALY7CGLl174-Fk7LGK4DvGeOu-FktIeE",
   authDomain: "app-forge-f7a07.firebaseapp.com",
   projectId: "app-forge-f7a07",
   storageBucket: "app-forge-f7a07.firebasestorage.app",
   messagingSenderId: "579204050538",
   appId: "1:579204050538:web:5e52487a75730d958e2413"
 };
+
 const app = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
